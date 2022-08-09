8beb8fb0024ca8dd1b15170beaa0c25281be55b9
sozi <sozyarnadir@gmail.com>
2022-08-09 22:58:24 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    return string.toUpperCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+    var no = "I can't hear you!";
+    var yes = "YES INDEED!";
+    var love = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+        return no;
+    } else if (string.toUpperCase(string) === string) {
+        return yes;
+    } else if ("Let's have dinner together! " === string) {
+        return love;
+    }
+}
